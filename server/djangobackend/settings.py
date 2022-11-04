e43fff54b0c42922ba05bc8f0a550c924ece7d30
Yuzdzhan Mehmedov <ynmehmedov@gmail.com>
2022-11-04 04:36:14 -0400

    Correction

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ["127.0.0.1","innovacar.mybluemix.net","localhost"]
+ALLOWED_HOSTS =  ["127.0.0.1","ynmehmedov.eu-gb.mybluemix.net","localhost"]
 
 
 # Application definition
